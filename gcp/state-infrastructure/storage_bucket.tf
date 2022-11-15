9a5cff6b3522a5dcb1556d31cdc38518a4cffa41
Julia Paiva <juliavpaiva@outlook.com>
2022-11-15 13:21:55 -0300

    Updating storage bucket

--- gcp/state-infrastructure/storage_bucket.tf
+++ gcp/state-infrastructure/storage_bucket.tf
@@ -1,9 +1,5 @@
-resource "google_storage_bucket" "bucket02" {
-
-  name = var.google_bucket_name
-
-  storage_class = var.google_storage_class
-
+resource "google_storage_bucket" "bucket-02-iac-trabalho-1" {
+  name     = var.google_bucket_name
   location = var.google_region
 
 }
