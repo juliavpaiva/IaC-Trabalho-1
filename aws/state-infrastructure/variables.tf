902c4b645936dcdc97f0a778c473529d69c9ed5c
Julia Paiva <juliavpaiva@outlook.com>
2022-11-15 08:33:16 -0300

    Adding state infrastructure workflow

---
+++ aws/state-infrastructure/variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+  description = "Deployed region definition"
+  default     = "us-east-1"
+}
+
+variable "aws_bucket_name" {
+  description = "Application name"
+  default     = "bucket01"
+}
+
+variable "aws_env" {
+  description = "Environment name"
+  default     = "dev"
+}
+
+variable "aws_repo" {
+  description = "Appplication repository"
+  default     = "https://github.com/juliavpaiva/IaC-Trabalho-1"
+}
