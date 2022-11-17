fb9a156a3f961c1962c7c96e2c470355ac1ea493
Julia Paiva <juliavpaiva@outlook.com>
2022-11-17 19:27:27 -0300

    Updating server 01 script

--- aws/main-infrastructure/ec2.tf
+++ aws/main-infrastructure/ec2.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "server01" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.aws_instance_type
 
-  user_data = file("./startup.sh")
+  user_data = file("./startup_script_server01.sh")
 
   tags = {
     Name = "Server01-${var.aws_env}"
